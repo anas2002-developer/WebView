36703e9ab07d79d1d06ef183b30fa5819ee1f3b3
anas2002 <anas4112002@gmail.com>
2023-03-17 00:48:58 +0530

    Added download manager

--- app/src/main/java/com/anas/webview/MainActivity.java
+++ app/src/main/java/com/anas/webview/MainActivity.java
@@ -1,18 +1,31 @@
 package com.anas.webview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.app.DownloadManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
+import android.webkit.DownloadListener;
+import android.webkit.URLUtil;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     WebView vWB;
     ProgressBar ProgressBar;
+
+    String url = "https://www.youtube.com/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
         vWB=findViewById(R.id.vWV);
         ProgressBar=findViewById(R.id.ProgressBar);
-        vWB.loadUrl("https://www.google.com/");
+        vWB.getSettings().setJavaScriptEnabled(true);
+        vWB.loadUrl(url);
 
         //for setting new urls in your old view
         vWB.setWebViewClient(new WebViewClient(){
@@ -41,6 +55,35 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        vWB.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimetype, long contentLength) {
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(url));
+                request.setTitle(URLUtil.guessFileName(url, contentDisposition, mimetype));
+                request.setDescription("Downloading file...");
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, URLUtil.guessFileName(url, contentDisposition, mimetype));
+                DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+                dm.enqueue(request);
+                Toast.makeText(getApplicationContext(), "Downloading...", Toast.LENGTH_SHORT).show();
+                registerReceiver(onComplete, new IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE));
+            }
+            BroadcastReceiver onComplete = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context context, Intent intent) {
+                    Toast.makeText(getApplicationContext(), "Downloading Complete", Toast.LENGTH_SHORT).show();
+                }
+            };
+        });
+
+    }
+
+    private void checkDownloadPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(MainActivity.this, "Write External Storage permission allows us to save files. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, 100);
+        }
     }
 
     @Override
